77d3729b548b9d253de0b17f5f9d49db19160e86
Aleksandra Pelse <shashka_aya@mail.ru>
2022-12-15 21:13:09 +0300

    remove unnecc

--- src/utils/databaseUtils.js
+++
@@ -1,8 +0,0 @@
-function localStorageListOf(key, defaultValue = []) {
-  const value = localStorage.getItem(key);
-  if (value === null) return defaultValue;
-  const parsedValue = JSON.parse(value);
-  return Array.isArray(parsedValue) ? parsedValue : defaultValue;
-}
-
-export {localStorageListOf};
