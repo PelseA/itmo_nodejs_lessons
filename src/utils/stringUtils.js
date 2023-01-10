fa6ca3bd154cb862fcfede060d5a596f22853419
Aleksandra Pelse <shashka_aya@mail.ru>
2023-01-10 18:59:34 +0300

    bookstore workflow

--- src/utils/stringUtils.js
+++
@@ -1,7 +0,0 @@
-function isStringNotNumberAndNotEmpty(value) {
-  const isValueString = typeof value === 'string';
-  const isValueNotNumber = isNaN(parseInt(value));
-  return isValueString && isValueNotNumber && value.length > 0;
-}
-
-export {isStringNotNumberAndNotEmpty};
