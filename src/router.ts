160ee8c9eca0dfb915348f3a053ba938d9ed2349
Aleksandra Pelse <shashka_aya@mail.ru>
2023-01-19 20:59:59 +0300

    add enter crlf for prettier validation

--- src/router.ts
+++ src/router.ts
@@ -3,25 +3,25 @@ import Routes from './consts/Routes';
 import IndexPage from './pages/IndexPage.vue';
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
-        {
-            path: Routes.INDEX,
-            component: IndexPage,
-        },
-        {
-            path: Routes.BOOKS,
-            component: () => import('./pages/BooksPage.vue'),
-        },
-        {
-            path: Routes.LOGIN,
-            component: () => import('./pages/LoginPage.vue'),
-        },
-        {
-            path: Routes.REGISTER,
-            component: () => import('./pages/RegisterPage.vue'),
-        },
-    ],
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: Routes.INDEX,
+      component: IndexPage,
+    },
+    {
+      path: Routes.BOOKS,
+      component: () => import('./pages/BooksPage.vue'),
+    },
+    {
+      path: Routes.LOGIN,
+      component: () => import('./pages/LoginPage.vue'),
+    },
+    {
+      path: Routes.REGISTER,
+      component: () => import('./pages/RegisterPage.vue'),
+    },
+  ],
 });
 
 export default router;
